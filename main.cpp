16501d3ae12dda2e92b2c383d26344e6bc28a799
wljss <2450805514@qq.com>
2026-06-03 10:50:56 +0000

    encode

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 #include<fstream>
 #include<cstdint>
 #include<string>
+#include<algorithm>
 
 struct Config //Llama2.c导出的二进制文件头部的28个字节是配置参数
 {
@@ -38,18 +39,19 @@ struct TransformerWeights //网络权重
 struct RunState
 {
     // vector相比new[]可以自己释放，性能上也差不多
-    std::vector<float> x; //TODO:
-    std::vector<float> xb; //
-    std::vector<float> xb2; //
-    std::vector<float> hb; //
-    std::vector<float> hb2; //
-    std::vector<float> q; //
+    std::vector<float> x; //各个变量的解释见allocate
+    std::vector<float> xb; 
+    std::vector<float> xb2; 
+    std::vector<float> hb; 
+    std::vector<float> hb2; 
+    std::vector<float> q; 
     float* k = nullptr; //后面会指向cache
     float* v = nullptr; //后面会指向cache
-    std::vector<float> att; //
-    std::vector<float> logits; //
-    std::vector<float> key_cache; //
-    std::vector<float> value_cache; //
+    std::vector<float> key_cache; 
+    std::vector<float> value_cache; 
+    std::vector<float> att; 
+    std::vector<float> logits; 
+    
 
     RunState()=default; //默认构造函数
     explicit RunState(const Config &p){allocate(p);} //带参数的构造函数。explicit的作用是禁止隐式类型转换
@@ -65,14 +67,14 @@ struct RunState
         hb.assign(static_cast<size_t>(p.hidden_dim), 0.0f);
         hb2.assign(static_cast<size_t>(p.hidden_dim), 0.0f);
 
-        q.assign(static_cast<size_t>(p.dim), 0.0f);
+        q.assign(static_cast<size_t>(p.dim), 0.0f); //query,dim=n_q_heads*head_size
 
-        key_cache.assign(static_cast<size_t>(p.n_layers) * p.seq_len * kv_dim, 0.0f);
-        value_cache.assign(static_cast<size_t>(p.n_layers) * p.seq_len * kv_dim, 0.0f);
+        key_cache.assign(static_cast<size_t>(p.n_layers) * p.seq_len * kv_dim, 0.0f); //每层每个位置缓存kv_dim维key
+        value_cache.assign(static_cast<size_t>(p.n_layers) * p.seq_len * kv_dim, 0.0f); //每层每个位置缓存kv_dim维value
 
-        att.assign(static_cast<size_t>(p.n_q_heads) * p.seq_len, 0.0f);
+        att.assign(static_cast<size_t>(p.n_q_heads) * p.seq_len, 0.0f); //每个query head对历史seq_len个位置的attention分数
 
-        logits.assign(p.vocab_size, 0.0f);
+        logits.assign(p.vocab_size, 0.0f); //输出词表上每个token的未归一化分数
     }
 };
 
@@ -149,6 +151,10 @@ struct TokenIndex //用来根据字符串找到token id
 {
     std::string str;
     int id;
+    friend bool operator <(const TokenIndex& a, const TokenIndex& b) // 按照字典序排序
+    { 
+        return a.str < b.str;
+    }
 };
 
 struct Tokenizer 
@@ -162,10 +168,6 @@ struct Tokenizer
 
     std::array<unsigned char, 512> byte_pieces{}; //储存256个单字节字符串，每个字符串占2个字节，例如'A'后面接一个'\0'
 };
-
-bool compare_tokens(const TokenIndex& a, const TokenIndex& b) { // 按照字典序排序
-    return a.str < b.str;
-}
 
 void build_tokenizer(Tokenizer& tokenizer, const std::string& tokenizer_path, int vocab_size) 
 {
@@ -254,9 +256,161 @@ void matmul(float* xout,const float* x,const float* w,int n,int d) // W(d,n)矩�
         xout[i]=val;
     }
 }
+void ensure_sorted_vocab(Tokenizer& tokenizer)
+{
+    if (!tokenizer.sorted_vocab.empty()) 
+    {
+        return;
+    }
+
+    tokenizer.sorted_vocab.reserve(static_cast<size_t>(tokenizer.vocab_size));
+
+    for (int i=0;i<tokenizer.vocab_size;++i) 
+    {
+        tokenizer.sorted_vocab.push_back(TokenIndex{tokenizer.vocab[i],i});
+    }
+
+    std::sort(tokenizer.sorted_vocab.begin(),tokenizer.sorted_vocab.end());
+}
+int str_lookup(const std::string& str,const Tokenizer& tokenizer) //二分查找str对应得token id
+{
+    auto it = std::lower_bound(tokenizer.sorted_vocab.begin(),tokenizer.sorted_vocab.end(),(TokenIndex){str,-1}); //TokenIndex默认比较字符串，赋值个-1没问题
+
+    if(it!=tokenizer.sorted_vocab.end()&&it->str==str) 
+    {
+        return it->id;
+    }
+
+    return -1;
+} 
+std::vector<int> encode(Tokenizer& tokenizer,const std::string& text,bool bos,bool eos) //这里的bos和eos是是否加入开始/结束标记
+{
+    ensure_sorted_vocab(tokenizer);
+
+    std::vector<int> tokens;
+    tokens.reserve(text.size()+3); //+3是BOS和EOS和dummy_prefix
+
+    if (bos) //可选添加BOS token，BOS=1
+    {
+        tokens.push_back(1);
+    }
+
+    if (!text.empty()) //SentencePiece风格的dummy prefix,非空文本前加一个空格 token 
+    {
+        int dummy_prefix=str_lookup(" ",tokenizer);
+        if(dummy_prefix==-1) 
+        {
+            throw std::runtime_error("dummy prefix token ' ' not found in vocabulary");
+        }
+
+        tokens.push_back(dummy_prefix);
+    }
+
+    //按UTF-8 codepoint初步编码。这是因为中文字符和表情等会占用多个字节，规律如下
+    /*
+        1字节字符: 0xxxxxxx
+        2字节字符: 110xxxxx 10xxxxxx
+        3字节字符: 1110xxxx 10xxxxxx 10xxxxxx
+        4字节字符: 11110xxx 10xxxxxx 10xxxxxx 10xxxxxx
+    */
+    std::string str_buffer; //用来处理UTF-8
+    str_buffer.reserve(4); //UTF-8单个codepoint最多4字节
+
+    for(size_t pos=0;pos<text.size();++pos) 
+    {
+        unsigned char byte=text[pos];
+        
+        if((byte&0xC0)!=0x80) //如果当前字节不是UTF-8 continuation byte，则开始新的codepoint
+        {
+            str_buffer.clear();
+        }
+
+        str_buffer.push_back(static_cast<char>(byte));
+
+        bool next_is_continuation = false;
+        if(pos+1<text.size()) 
+        {
+            next_is_continuation=((text[pos+1]&0xC0)==0x80);
+        }
+
+        
+        if(next_is_continuation && str_buffer.size()<4) //如果下一个字节还是continuation byte，并且当前codepoint长度还没超过4，说明这个UTF-8 codepoint还没读完，继续累积
+        {
+            continue;
+        }
+        else //否则说明已经拿到一个完整codepoint，尝试在词表中查找
+        {
+            int id=str_lookup(str_buffer,tokenizer);
+
+            if(id!=-1) //找到了
+            {
+                tokens.push_back(id);
+            } 
+            else //没找到，退化成按字节编码
+            {
+                
+                for (unsigned char b : str_buffer) 
+                {
+                    tokens.push_back(static_cast<int>(b) + 3); //每个原始字节映射到byte+3是因为前三个一般是<unk> BOS EOS
+                }
+            }
+
+            str_buffer.clear();
+        }
+    }
+
+    
+    while (true) //BPE合并,每轮选择vocab_scores最高的可合并pair
+    {
+        float best_score=-1e10f;
+        int best_id=-1; //token id
+        int best_pos=-1; //在token序列中的位置
+
+        for(int i=0;i<static_cast<int>(tokens.size())-1;++i) 
+        {
+            const std::string& left=tokenizer.vocab[tokens[i]];
+            const std::string& right=tokenizer.vocab[tokens[i+1]];
+
+            std::string merged=left+right;
+
+            int id=str_lookup(merged,tokenizer);
+
+            if(id!=-1&&tokenizer.vocab_scores[id]>best_score) 
+            {
+                best_score=tokenizer.vocab_scores[id];
+                best_id=id;
+                best_pos=i;
+            }
+        }
+
+        if(best_pos==-1) //所有相邻pair的合并string都在词表中找不到
+        {
+            break;
+        }
+        
+        tokens[best_pos]=best_id; //把tokens[best_pos]和tokens[best_pos+1]合并成best_id
+        tokens.erase(tokens.begin()+best_pos+1);
+    }
+
+    
+    if(eos) //可选添加EOS token，EOS=2
+    {
+        tokens.push_back(2);
+    }
+
+    return tokens;
+}
+
+
+
 void generate(Transformer &transformer,Tokenizer &tokenizer,Sampler &sampler,const std::string &prompt,int steps)
 {
-    printf("Welcome!");
+    std::vector<int>prompt_tokens=encode(tokenizer,prompt,true,false); //把prompt编码成token序列
+
+    for(auto token : prompt_tokens)
+    {
+        printf("token : %d\n",token);
+    }
 }
 
 bool read_checkpoint_header_and_map(Config &config,const std::string filename,float* &data,int &fd,ssize_t &file_size)
